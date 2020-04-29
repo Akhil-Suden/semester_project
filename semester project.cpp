667243f6dbd3a9d825345f06b42f9d7cee462328
Akhil Suden <59475385+Akhil-Suden@users.noreply.github.com>
2020-04-29 19:10:47 +0530

    Add files via upload

---
+++ semester project.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class device{
+public:
+int c;
+makeconn(){
+int arr[c];	
+for(int i=0;i<c;i++){
+cout<<"Enter connected device ID :";
+cin>>arr[i];
+}	
+}
+};
+int main(){
+int n;
+cout<<"Enter No. of end devices :";
+cin>>n;	
+device arr[n];
+for(int i=0;i<n;i++){
+cout<<"Enter no. of connections for device "<<i+1<<" :";
+cin>>arr[i].c;
+arr[i].makeconn();
+getch();
+}
+}
