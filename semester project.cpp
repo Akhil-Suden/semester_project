da37a88a216103757c7bf7a166c85f9789556605
Akhil Suden <59475385+Akhil-Suden@users.noreply.github.com>
2020-04-29 23:49:21 +0530

    physical layer
    send function added

--- semester project.cpp
+++ semester project.cpp
@@ -1,26 +1,39 @@
+//physical layer implementation
 #include<iostream>
-#include<conio.h>
 using namespace std;
+int n;
 class device{
 public:
 int c;
-makeconn(){
-int arr[c];	
+int conn[30];	
+void makeconn(){
 for(int i=0;i<c;i++){
-cout<<"Enter connected device ID :";
-cin>>arr[i];
-}	
+cout<<"Enter connected device ID ("<< 0 <<"to"<<n-1<<")"<<endl;
+cin>>conn[i];
+}	}
+void send(string data){
+for(int i=0;i<c;i++)
+cout<<conn[i]<<" recieved data \""<<data<<"\""<<endl; 
+
 }
 };
 int main(){
-int n;
+int a;
 cout<<"Enter No. of end devices :";
 cin>>n;	
-device arr[n];
+device dev[n];
 for(int i=0;i<n;i++){
 cout<<"Enter no. of connections for device "<<i+1<<" :";
-cin>>arr[i].c;
-arr[i].makeconn();
-getch();
+cin>>dev[i].c;
+dev[i].makeconn();
 }
+cout<<"All connection are made ."<<endl;
+cout<<"Enter sending station ID "<<endl;
+cin>>a;
+string data;
+cout<<"Enter DATA to send "<<endl;
+getline(cin,data);
+getline(cin,data);
+dev[a].send(data);
+
 }
